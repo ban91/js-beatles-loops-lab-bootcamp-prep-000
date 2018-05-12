aca8a70017bcb52415297bd8f336b99c29894ea8
Brittany Nieves <neopetlover912002@yahoo.com>
2018-05-12 20:28:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(music, instr){
   var band = [];
-  for (var i = 0; i < 4; i++) {
+  for (let i = 0; i < 4; i++) {
   band.push(`${music[0]} plays ${instr[0]}`);
 }
 return band
