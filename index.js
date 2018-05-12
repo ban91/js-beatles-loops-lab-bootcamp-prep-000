759920d40f54cf8a3d7c05129f389ee8b9060165
Brittany Nieves <neopetlover912002@yahoo.com>
2018-05-12 20:33:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-function theBeatlesPlay(musicians, instruments) {
-  var array = [];
-  for (let i = 0; i < 4; i++) {
-      array.push(`${musicians[0]} plays ${instruments[0]}`);
-    }
-  return array;
+function theBeatlesPlay(music, instr){
+  var band = [];
+  for (let i = 0; i < 4; i++){
+  band.push(`${music[0]} plays ${instr[0]}`);
+}
+return band;
 }
