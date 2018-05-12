1a521fb7dcecaf426f1dc1deb99015d619d24a31
Brittany Nieves <neopetlover912002@yahoo.com>
2018-05-12 20:36:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const music = ["John", "Paul", "George", "Ringo"];
-
-const instr = ["guitar", "guitar", "bass", "drums"];
 
 function theBeatlesPlay(music, instr){
   var band = [];
   for (let i = 0; i < 4; i++){
-  band.push(`${music[0]} plays ${instr[0]}`);
+  band.push(`${music[i]} plays ${instr[i]}`);
 }
 return band;
 }
